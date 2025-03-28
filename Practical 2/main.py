c300170c7531c4364aca4b2bd9c371ee4325df63
tijmenbee <75034218+tijmenbee@users.noreply.github.com>
2025-03-28 14:44:22 +0100

    Cleanup

--- Practical 2/main.py
+++ Practical 2/main.py
@@ -4,26 +4,10 @@ import time
 import sys
 #iterative_local_search(G, 0.2, 10000)
 #multi_start_local_search(G, 10000)
+GLS = genetic_local_search(G, 50, 10000, False)
+GLS_ILS = genetic_local_search(G, 8, 10000, False, True, 0.2)
+print(GLS.fitness)
+print(GLS_ILS.fitness)
 
 
-GLS = []
-i = 0
-best_run = 1000
-avg_run = 999
-pop_size = 70
-iteration = 10
-while avg_run < best_run:
-    GLS_ILS = []
-    for j in range(iteration):
-        GLS_ILS.append(genetic_local_search(G, pop_size,10000, False, False, 0.2).fitness)
-        sys.stdout.write("Run ... %s%%\r" % ((j/iteration)*100))
-        sys.stdout.flush()
-
-    #GLS.append(genetic_local_search(G, 50,10000,False ,False, 0.2).fitness)
-    print(f"run: {i}, pop_size: {pop_size}, avg: {sum(GLS_ILS)/iteration}, min: {min(GLS_ILS)}")
-    pop_size -= 5
-    avg_run = sum(GLS_ILS)/10
-    i += 1
-print(GLS_ILS)
-print(GLS)
 
